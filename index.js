77c7f23a38f84c193d4a9c051db6833ee898c6ff
Lewis <73433155+lewisconnett@users.noreply.github.com>
2024-02-24 17:48:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function (e) {
+    var btn = document.querySelector("#submit");
+    var result = document.querySelector("#result");
+
+    btn.addEventListener("click", function (e) { 
+        const url = "http://mw159.brighton.domains/ci527/tut5/data.txt";
+        
+    })
+        
+
+});
